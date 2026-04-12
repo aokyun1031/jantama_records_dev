2031430160cef52f54b15e821dfbcf74895e1b09
aokyun1031 <aokyun1031@gmail.com>
2026-04-12 23:04:37 +0900

    [main] - [update] : 初回起動時に失敗したため自動的にcomposerのインストールを行うようにする

--- start.sh
+++ start.sh
@@ -6,6 +6,11 @@ if [ ! -f phinx.php ] && [ -f phinx.php.example ]; then
   cp phinx.php.example phinx.php
 fi
 
+# vendorが無ければcomposer installを実行（ボリュームマウントで上書きされた場合の対処）
+if [ ! -f vendor/bin/phinx ]; then
+  composer install --no-dev --no-interaction --optimize-autoloader
+fi
+
 # デプロイ時にPhinxマイグレーションを自動実行
 php vendor/bin/phinx migrate
 
